1fc1c61d08d034e62e3b2e73b64bc2f449d35ab0
Simon Oz <11583612+OzSimon@users.noreply.github.com>
2024-08-01 22:17:04 +1000

    Modules Added

---
+++ Git.ps1
@@ -0,0 +1,5 @@
+git clone --recurse-submodules https://github.com/OzSimon/Microsoft-365.git
+git submodule add https://github.com/OzSimon/Module-Exchange-Online.git
+git submodule add https://github.com/OzSimon/Module-SharePoint-Online.git
+git submodule init
+git submodule update
