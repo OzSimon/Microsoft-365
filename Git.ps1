856d2cfb5954d7f94fafcd21627be92748060bd6
Simon Oz <11583612+OzSimon@users.noreply.github.com>
2024-08-08 06:07:40 +1000

    Git Updated

--- Git.ps1
+++ Git.ps1
@@ -1,5 +1,8 @@
 git clone --recurse-submodules https://github.com/OzSimon/Microsoft-365.git
 git submodule add https://github.com/OzSimon/Module-Exchange-Online.git
 git submodule add https://github.com/OzSimon/Module-SharePoint-Online.git
+git submodule add https://github.com/OzSimon/Module-Teams.git
 git submodule init
 git submodule update
+
+git filter-branch -f --env-filter "GIT_AUTHOR_NAME='Simon Oz'; GIT_AUTHOR_EMAIL='11583612+OzSimon@users.noreply.github.com'; GIT_COMMITTER_NAME='Simon Oz'; GIT_COMMITTER_EMAIL='11583612+OzSimon@users.noreply.github.com';" HEAD
